bf767068e0d466098737f6806990460b49f1fafd
dantoso <xeddantasvfd@gmail.com>
2022-02-22 17:03:55 -0300

    init

---
+++ Peptobismol/Peptobismol/PeptobismolApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeptobismolApp.swift
+//  Peptobismol
+//
+//  Created by Matheus Dantas on 22/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PeptobismolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
