81d4a9915f83a56a55d509e7b3ba276ccbf9913a
PhilippeVenancio <87039791+PhilippeVenancio@users.noreply.github.com>
2022-02-24 14:51:34 -0300

    feat: just the timer

--- Peptobismol/View/Screens/Timer/TimerView.swift
+++ Peptobismol/View/Screens/Timer/TimerView.swift
@@ -8,8 +8,17 @@
 import SwiftUI
 
 struct TimerView: View {
+    @State var currentDate = Date()
+    
+    //common: Deixa o processo em loop
+    //autonnect: Se conecta automaticamente ao seu editor conectável upstream.
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("\(currentDate)")
+            .onReceive(timer) { input in
+                currentDate = input
+            }
     }
 }
 
